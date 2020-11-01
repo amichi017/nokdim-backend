498db939783a82fb670ebac80ad2263a607b5745
amichi017 <amichi12345678@gmail.com>
2020-11-01 21:18:00 +0200

    v1

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const http = require('http');
 const app = require('./app')
 
 const server =http.createServer(app);
-
-server.listen(process.env.PORT || 5000);
+const port =process.env.PORT || 5000; 
+server.listen(port);
 
