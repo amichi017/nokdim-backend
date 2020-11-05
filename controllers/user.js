045f2a9b08efa3ffb3f3084939dcb4df404138a9
amichi017 <amichi12345678@gmail.com>
2020-11-05 14:45:41 +0200

    v1

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
-const { deleteOne } = require('../models/user');
+
 
 module.exports={
     login:(req, res) => {
